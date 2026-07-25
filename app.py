972ae0ee354f97341bd640468410c3d2ec20daf2
sruthinandhana14-stack <sruthinandhana14@gmail.com>
2026-07-25 14:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os, re, urllib.parse, urllib.request
 from flask import Flask, abort, jsonify, render_template, request
 
 app = Flask(__name__)
-
-
 def get_vid(q):  
     try:
         req = urllib.request.Request(f"https://www.youtube.com/results?search_query={urllib.parse.quote(q)}", headers={"User-Agent": "Mozilla/5.0"})
